76a44adee504dd6b0c48bc965af17aefb88ea261
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-22 20:39:31 +0300

    reflection example create instance

--- src/main/java/ru/philimonov/springcourse/reflect/ReflectionExample.java
+++ src/main/java/ru/philimonov/springcourse/reflect/ReflectionExample.java
@@ -4,17 +4,20 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class ReflectionExample {
-    public static void main(String[] args) {
-        Method[] methods = Arrays.class.getMethods();
-        for (Method method : methods) {
-            System.out.println(method);
-        }
-
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+//        Method[] methods = Arrays.class.getMethods();
+//        for (Method method : methods) {
+//            System.out.println(method);
+//        }
+//
         Runnable runnable = new Runnable(){
             public void run(){
                 System.out.println("Hello world");
             }
         };
         System.out.println(runnable.getClass().getName());
+        Runnable runnable1 = runnable.getClass().newInstance();
+        System.out.println(runnable1);
+
     }
 }
