cbad570ff47932f35ca9584a4692a24c6d9fd78c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-22 20:11:41 +0300

    add prop.properties

--- src/main/java/ru/philimonov/springcourse/beans/Cat.java
+++ src/main/java/ru/philimonov/springcourse/beans/Cat.java
@@ -1,19 +1,21 @@
 package ru.philimonov.springcourse.beans;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@PropertySource("classpath:prop.properties")
 @Component
 public class Cat {
-    @Value("Boris")
+    @Value("${cat.name}")
     private String name;
 
-    @Value("2")
+    @Value("${cat.age}")
     private int age;
 
-    @Value(value = "#{'Mahsa,Lisa'.split(',')}")
+    @Value("${cat.girls}")
     private List<String> girls;
 
     @Override
